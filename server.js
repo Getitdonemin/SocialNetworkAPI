60c0c567d31477c67c8912234c1ae04225e30a5a
Your Name <getitdonemin@gmail.com>
2022-12-13 10:58:59 -0600

    last but not least added our server

---
+++ server.js
@@ -0,0 +1,22 @@
+// express and mongoose are needed
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+// Connecting mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// this will log all mongoose queries
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(` API server running on port ${PORT}`));
